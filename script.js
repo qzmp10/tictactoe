a2474b8c0e1c01a57e1a726644bb0301fe84d3e6
qzmp10 <loganrobitaille@outlook.com>
2022-06-08 18:06:10 -0400

    Added comments of factory functions structure

--- script.js
+++ script.js
@@ -1,3 +1,52 @@
+//1. make the html & css of the tic tac toe board
+//2. make the gameplay aspect (X's & O's appear on the board)
+//3. make the storing game state aspect (multiple X's and O's)
+//4. keep the players scores
+
+//5. 
+
 const Gameboard = () => {
-    
-}
+    let gameboardState = [];
+};
+
+const PlayerScores = () => {
+
+};
+
+const PlayerTurn = () => {
+
+};
+
+
+// ------------------------------------------------------------
+
+// -- examples of modules & factory functions -- //
+
+// let myModule = (function() {
+//     let privateSecret = 'osmosis GOT RUGGED!';
+//     let _displayMessage = function() {
+//         for (i = 0; i < 10; i++) {
+//             console.log(privateSecret);
+//         }
+//     }
+//     return {
+//         publicMsgCall: function() {
+//             _displayMessage();
+//         }
+//     }
+// })();
+
+// myModule.publicMsgCall();
+
+// -----------------------------------------------------------
+
+// let Veggie = (vegetable) => {
+//     let whichVeggie =  function() {
+//         console.log(vegetable);
+//     }
+//     return { whichVeggie };
+// };
+
+// let whichFknVeggie = Veggie('carrot');
+
+// whichFknVeggie.whichVeggie();
